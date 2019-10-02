78d7647af77b568f12e1179140a3caa28c094a07
Stephen Jong <stephen_jong@qbsol.com>
2019-10-02 07:21:01 -0500

    set meat image to bind to image in viewmodel; added in future classes for meat/webresult from web project

---
+++ Carne/Models/Meat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Carne.Models
+{
+    public class Meat
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Restaurant { get; set; }
+        public double Price { get; set; }
+        public string URI { get; set; }
+    }
+}
---
+++ Carne/Models/WebResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Carne.Models
+{
+    public class WebResult
+    {
+        public string Status { get; set; }
+    }
+}
